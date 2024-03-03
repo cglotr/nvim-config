0f8b5a343450018e16fc2937fc1e39e08ea51e2e
cglotr <aaryfn@gmail.com>
2024-03-03 02:51:00 -0800

    telescope

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,3 +1,8 @@
 return {
-    { "rose-pine/neovim", name = "rose-pine" }
+    { "rose-pine/neovim", name = "rose-pine" },
+    {
+        'nvim-telescope/telescope.nvim',
+        tag = '0.1.5',
+        dependencies = { 'nvim-lua/plenary.nvim' }
+    }
 }
