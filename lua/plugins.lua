e35d399221fe05fc53d6fb083585f84b77a3c68f
cglotr <aaryfn@gmail.com>
2024-03-03 03:05:25 -0800

    treesitter

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -4,5 +4,12 @@ return {
         'nvim-telescope/telescope.nvim',
         tag = '0.1.5',
         dependencies = { 'nvim-lua/plenary.nvim' }
+    },
+    {
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate"
+    },
+    {
+        'nvim-treesitter/playground'
     }
 }
