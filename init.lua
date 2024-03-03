2a7b8e8a208c1ff41d78fec5934237f606240c99
cglotr <aaryfn@gmail.com>
2024-03-02 18:02:43 -0800

    init

---
+++ init.lua
@@ -0,0 +1,6 @@
+require('cglotr.global')
+require('cglotr.option')
+require('cglotr.keymap')
+require('cglotr.lazy')
+
+vim.cmd("colorscheme rose-pine")
---
+++ lua/cglotr/keymap.lua
@@ -0,0 +1,3 @@
+vim.keymap.set('n', '<leader>v', vim.cmd.Ex)
+vim.keymap.set('n', '<leader>s', vim.cmd.Sex)
+vim.keymap.set('n', '<leader>q', vim.cmd.q)
