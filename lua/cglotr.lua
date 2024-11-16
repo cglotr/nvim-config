875a2130d61b5afc8ae8a5fc6ce52a3a86170601
cglotr <aaryfn@gmail.com>
2024-11-16 11:55:12 -0800

    tabstop & harpoon keymap

--- lua/cglotr.lua
+++ lua/cglotr.lua
@@ -6,6 +6,7 @@ vim.o.nu = true
 vim.o.rnu = true
 vim.o.termguicolors = true
 vim.o.colorcolumn = "80,100,120"
+vim.o.tabstop = 4
 
 -- https://neovim.io/doc/user/pi_netrw.html#netrw-variables
 vim.g.netrw_banner = 0
