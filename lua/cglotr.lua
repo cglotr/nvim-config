5de6c2b307de69045170e551bafe451ba7eb1ea9
cglotr <aaryfn@gmail.com>
2024-11-16 07:46:31 -0800

    nu

--- lua/cglotr.lua
+++ lua/cglotr.lua
@@ -2,8 +2,9 @@ vim.g.mapleader = " "
 
 vim.keymap.set("n", "<leader>v", vim.cmd.Ex)
 
+vim.o.nu = true
 vim.o.rnu = true
 
 -- https://neovim.io/doc/user/pi_netrw.html#netrw-variables
 vim.g.netrw_banner = 0
-vim.g.netrw_bufsettings = "noma nomod nonu nowrap ro nobl"
+vim.g.netrw_bufsettings = "noma nomod nu nowrap ro nobl"
