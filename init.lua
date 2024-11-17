b81cb9e1c43d6fbab6a5c4eddd7db2bd3a5327ea
cglotr <aaryfn@gmail.com>
2024-11-16 16:08:05 -0800

    telescope

--- init.lua
+++ init.lua
@@ -2,5 +2,6 @@ require("cglotr")
 
 require("config.lazy")
 require("config.harpoon")
+require("config.telescope")
 
 vim.cmd.colorscheme("rose-pine")
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,8 +1,15 @@
 return {
-    { "rose-pine/neovim", name = "rose-pine" },
+    { 
+		"rose-pine/neovim",
+		name = "rose-pine",
+	},
     {
         "ThePrimeagen/harpoon",
         branch = "harpoon2",
         dependencies = { "nvim-lua/plenary.nvim" }
-    }
+    },
+	{
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.8",
+	}
 }
