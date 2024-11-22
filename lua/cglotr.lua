289bd243f2ae3d8e7943b668d554784bdb6da837
cglotr <aaryfn@gmail.com>
2024-11-21 22:13:43 -0800

    show whitespace

--- lua/cglotr.lua
+++ lua/cglotr.lua
@@ -9,6 +9,8 @@ vim.o.colorcolumn = "80,100,120"
 vim.o.tabstop = 4
 vim.o.shiftwidth = 4
 vim.o.expandtab = true
+vim.o.list = true
+vim.o.listchars = "tab:―→,trail:·,nbsp:+,space:·"
 
 -- https://neovim.io/doc/user/pi_netrw.html#netrw-variables
 vim.g.netrw_banner = 0
