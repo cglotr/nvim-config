a4266409593b92dbf966ba740b7ae23e5234f796
cglotr <aaryfn@gmail.com>
2024-11-21 22:18:46 -0800

    nicer tab

--- lua/cglotr.lua
+++ lua/cglotr.lua
@@ -10,7 +10,7 @@ vim.o.tabstop = 4
 vim.o.shiftwidth = 4
 vim.o.expandtab = true
 vim.o.list = true
-vim.o.listchars = "tab:―→,trail:·,nbsp:+,space:·"
+vim.o.listchars = "tab:· ,trail:·,nbsp:+,space:·"
 
 -- https://neovim.io/doc/user/pi_netrw.html#netrw-variables
 vim.g.netrw_banner = 0
