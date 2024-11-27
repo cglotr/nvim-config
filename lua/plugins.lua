f35bfc25108767adc3781ceb9eac9e77b077fbc7
cglotr <aaryfn@gmail.com>
2024-11-27 13:05:15 -0800

    install treesitter

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -8,6 +8,12 @@ return {
         branch = "harpoon2",
         dependencies = { "nvim-lua/plenary.nvim" }
     },
+    {
+        "nvim-treesitter/nvim-treesitter",
+        build = function()
+            require("nvim-treesitter.install").update({ with_sync = true })()
+        end,
+    },
 	{
 		"nvim-telescope/telescope.nvim",
 		tag = "0.1.8",
